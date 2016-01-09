39b2dc3d26150f59348cd023272c124f92630cc3
Bryan Willis <bryanwillis1@gmail.com>
2016-01-09 02:00:24 -0600

    / ‘vendor/config.php’

---
+++ vendor/config.php
@@ -0,0 +1,14 @@
+<?php
+
+
+/* // Load All Plugins
+foreach ( glob( plugin_dir_path( __FILE__ ) . "vendor/*.php" ) as $file ) {
+    include_once $file;
+}
+// */
+
+
+
+
+# Kirki Customizr Toolkit
+include_once( dirname( __FILE__ ) . '/plugins/kirki/kirki.php' );

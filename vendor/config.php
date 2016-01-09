2fbb9fedaebdb9dde045a8e3ace43a59ee66a671
Bryan Willis <bryanwillis1@gmail.com>
2016-01-09 02:05:51 -0600

    / ‘vendor/config.php’

--- vendor/config.php
+++ vendor/config.php
@@ -8,7 +8,12 @@ foreach ( glob( plugin_dir_path( __FILE__ ) . "vendor/*.php" ) as $file ) {
 // */
 
 
+# Advanced Custom Fields Pro
+DEFINE('BW_ACF_SETTINGS_PATH', '/acf/'); // ACF path relative to this file
+DEFINE('ACF_5_KEY', 'b3JkZXJfaWQ9NzIyNzZ8dHlwZT1wZXJzb25hbHxkYXRlPTIwMTYtMDEtMDkgMDU6MDA6MTU='); // ACF Pro Key
+include_once( dirname( __FILE__ ) . '/vendor/advanced-custom-fields-pro.php' );
+// add_filter('acf/settings/show_admin', '__return_false'); //Hide ACF field group menu item
 
 
 # Kirki Customizr Toolkit
-include_once( dirname( __FILE__ ) . '/plugins/kirki/kirki.php' );
+include_once( dirname( __FILE__ ) . '/vendor/kirki/kirki.php' );

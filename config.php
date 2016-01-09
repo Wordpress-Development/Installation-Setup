f1d73ee8d905378f379f769197d4036cdd06ecb4
Bryan Willis <willis.bryan@live.com>
2016-01-09 00:27:22 -0600

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,9 @@
 <?php
 
+
+# Advanced Custom Fields
 include_once( plugin_dir_url( __FILE__ ) . '/addons/acf/acf.php' );
+
+# Kirki Customizr Toolkit
+include_once( dirname( __FILE__ ) . '/plugins/kirki/kirki.php' );
+

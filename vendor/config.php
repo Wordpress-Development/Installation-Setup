7f0e23afc68f1faf9ae9bec765f54954d041787c
Bryan Willis <bryanwillis1@gmail.com>
2016-01-09 03:05:08 -0600

    / ‘vendor/config.php’

--- vendor/config.php
+++ vendor/config.php
@@ -2,14 +2,12 @@
 
 # Load Modules / Plugins in autoload folder
 define( 'BOOT_LOAD_MODULES', 1 )
-// include_once( dirname( __FILE__ ) . '/vendor/advanced-custom-fields-pro.php' );
-// include_once( dirname( __FILE__ ) . '/vendor/kirki/kirki.php' );
+include_once( dirname( __FILE__ ) . '/config/advanced-custom-fields-pro.php' );
+include_once( dirname( __FILE__ ) . '/config/template-loader.php' );
 
     
 # Advanced Custom Fields Pro
-DEFINE('BW_ACF_SETTINGS_PATH', '/acf/'); // ACF path relative to this file
+$acf_real_path = "realpath(__DIR__ . '/./config.php')";
+DEFINE('BW_ACF_SETTINGS_PATH', $acf_real_path); // ACF path relative to this file
 DEFINE('ACF_5_KEY', 'b3JkZXJfaWQ9NzIyNzZ8dHlwZT1wZXJzb25hbHxkYXRlPTIwMTYtMDEtMDkgMDU6MDA6MTU='); // ACF Pro Key
 // add_filter('acf/settings/show_admin', '__return_false'); //Hide ACF field group menu item
-
-
-

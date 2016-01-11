13501dc26a0353721261948e4de7aea86a04f633
Bryan Willis <bryanwillis1@gmail.com>
2016-01-10 19:56:31 -0600

    / ‘functions.php’

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Theme Plugin Functions
- *
  */
 
-// add_action( 'do_meta_boxes', 'remove_slider_rev_metabox' );
+
+// add_action( 'do_meta_boxes', 'remove_slider_rev_metabox' ); // Remove revolution slider metabox

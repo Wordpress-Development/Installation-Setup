f04dd05e6d6ed5473a471d0e665999ff84bcdf00
Bryan Willis <willis.bryan@live.com>
2016-01-30 22:43:25 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,4 +4,7 @@
  */
 
 
+//* ACF PRO LICENSE KEY
+DEFINE('ACF_5_KEY', 'b3JkZXJfaWQ9NzIyNzZ8dHlwZT1wZXJzb25hbHxkYXRlPTIwMTYtMDEtMDkgMDU6MDA6MTU=');
+
 // add_action( 'do_meta_boxes', 'remove_slider_rev_metabox' ); // Remove revolution slider metabox

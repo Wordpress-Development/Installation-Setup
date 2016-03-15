c126b97a8bd6a84fcf1c217654bfab2fe62a8b1c
Bryan Willis <willis.bryan@live.com>
2016-03-15 16:28:50 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -13,3 +13,15 @@ DEFINE('REVSLIDER_KEY', 'a42aae42-f225-4770-9644-b50609fc1fa3');
 
 
 // add_action( 'do_meta_boxes', 'remove_slider_rev_metabox' ); // Remove revolution slider metabox
+
+
+add_action( 'init', function() {
+    $user_name    = wp_get_current_user()->user_login;
+	  if( $user_name != 'bryanwillis' ) {
+		  defined('DISALLOW_FILE_EDIT') || define( 'DISALLOW_FILE_EDIT', true );
+		  defined('DISALLOW_FILE_MODS') || define( 'DISALLOW_FILE_MODS', true );
+	  } else {
+		  define( 'DISALLOW_FILE_EDIT', false );
+		  define( 'DISALLOW_FILE_MODS', false );
+	  }
+});
